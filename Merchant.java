bbac7166da575912270c6871e27c6ac58b87c525
AlexanderBradley1 <alexander.bradley884@gmail.com>
2022-04-19 20:15:22 -0600

    missed a file on early commit. sorry

--- Merchant.java
+++ Merchant.java
@@ -1,3 +1,44 @@
+import java.util.Arrays;
+
 public class Merchant {
-    
+    private int[] priceOfItem;
+    private String[] itemsForSale;
+    private String shopName;
+    private String greeting;
+    private String farewell;
+    private String errorMessage;
+    private String[] thingsToDo;
+
+    Merchant(String[] itemsForSale, int[] priceOfItem, String shopName, String[] thingsToDo, String greeting, String farewell, String errorMessage) {
+        this.itemsForSale = itemsForSale;
+        this.priceOfItem = priceOfItem;
+        this.shopName = shopName;
+        this.greeting = greeting;
+        this.farewell = farewell;
+        this.thingsToDo = thingsToDo;
+        this.errorMessage = errorMessage;
+    }
+
+    public void shop() {
+        System.out.println("-----------------------------------------------------------");
+        Printer.print(greeting);
+        Printer.print("What would you like to do: "+ Arrays.toString(thingsToDo));
+        int responseBack = ErrorChecker.compareArrayOfStrings(thingsToDo, errorMessage);
+        
+        switch (responseBack) {
+            case 1: //buy
+                System.out.println("Items for sale: ");
+                for (int i = 0; i < itemsForSale.length; i++) {
+                    System.out.println(itemsForSale[i]+ " for " + priceOfItem[i] + " grams of gold");
+                }
+                
+                break;
+            case 2:
+        
+            default:
+                break;
+        }
+        
+        
+    }
 }
--- Printer.java
+++ Printer.java
@@ -1,19 +1,19 @@
 public class Printer {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_WHITE = "\u001B[37m";
 
     /**
      * This method prints out a string one word at a time, in white. 
      * @param whatToPrint = the thing we print 1 word at a time. 
      */
-    public void print (String whatToPrint){
+    public static void print (String whatToPrint){
         //splits the words at all the spaces
         String words[] = whatToPrint.split(" ");
 

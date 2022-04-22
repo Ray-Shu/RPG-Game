f6c1f4febc6f3a1fdd9bc7b551b0b680c04b3b8b
Ray_Shu <rayshu1015@gmail.com>
2022-04-21 22:32:06 -0600

    test1

--- Player.java
+++ Player.java
@@ -1,27 +1,30 @@
 public class Player {
-    final String CLASSES[] = {"cyborg", "hacker", "terminator", "lazer swordsman", "rogue", "mystic"};
+    final String CLASSES[] = { "cyborg", "hacker", "terminator", "lazer swordsman", "rogue", "mystic" };
 
-    //Creates the player by asking questions and stuff. Chooses classes
-    public void classSelection(){
+    // Creates the player by asking questions and stuff. Chooses classes
+    public void classSelection() {
         Printer.printColor("Which class would you like to be?", "purple");
-        
+
         // * Prints the stats for all of the classes
-        //Cyborg
+        // Cyborg
         System.out.println();
-        Printer.printColor("Cyborg" + '\n' + "HP: 200" + '\n' + "MP: 100" + '\n' + "SPD: 1.00" + '\n' + "ATK: 5" + '\n' + "Physical Damage: 5" + '\n' + "Magic Damage: 0", "blue");
+        Printer.printColor("Cyborg" + '\n' + "HP: 200" + '\n' + "MP: 100" + '\n' + "SPD: 1.00" + '\n' + "ATK: 5" + '\n'
+                + "Physical Damage: 5" + '\n' + "Magic Damage: 0", "blue");
         System.out.println('\n' + "--------------------------------------------------");
 
-        //Hacker 
+        // Hacker
         System.out.println();
-        Printer.printColor("Hacker" + '\n' + "HP: 300" + '\n' + "MP: 80" + '\n' + "SPD: 0.75" + '\n' + "ATK: 4" + '\n' + "Physical Damage: 3" + '\n' + "Magic Damage: 3", "green");
+        Printer.printColor("Hacker" + '\n' + "HP: 300" + '\n' + "MP: 80" + '\n' + "SPD: 0.75" + '\n' + "ATK: 4" + '\n'
+                + "Physical Damage: 3" + '\n' + "Magic Damage: 3", "green");
         System.out.println('\n' + "--------------------------------------------------");
 
-        //Terminator
+        // Terminator
         System.out.println();
-        Printer.printColor("Terminator" + '\n' + "HP: 250" + '\n' + "MP: 90" + '\n' + "SPD: 0.70" + '\n' + "ATK: 5" + '\n' + "Physical Damage: 6" + '\n' + "Magic Damage: 0", "red");
+        Printer.printColor("Terminator" + '\n' + "HP: 300" + '\n' + "MP: 90" + '\n' + "SPD: 0.70" + '\n' + "ATK: 5"
+                + '\n' + "Physical Damage: 6" + '\n' + "Magic Damage: 0", "red");
         System.out.println('\n' + "--------------------------------------------------");
 
-        String chosenClass = ErrorChecker.compareArrayOfStrings(CLASSES, "Please choose a valid class", "purple"); 
+        String chosenClass = ErrorChecker.compareArrayOfStrings(CLASSES, "Please choose a valid class", "purple");
         switch (chosenClass) {
             // * string param must be all lower-case
             case "cyborg":
@@ -42,7 +45,7 @@ public class Player {
             case "mystic":
                 Printer.printColor("You chose Mystic.", "cyan");
                 break;
-            
+
         }
     }
 }

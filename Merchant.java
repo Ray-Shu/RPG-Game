c312498c078b7c84ffb8e76f8b82c331b6c2845b
AlexanderBradley1 <alexander.bradley884@gmail.com>
2022-04-24 21:15:53 -0600

    Made town and merchant classes look better: Added comments and made it neat

--- Merchant.java
+++ Merchant.java
@@ -3,29 +3,26 @@ import java.util.Scanner;
 
 public class Merchant {
     public int[] priceOfItem;
-    public String[] itemsForSale;
-    public String shopName;
-    public String greeting;
-    public String farewell;
-    public String errorMessage;
-    public String[] thingsToDo;
-    public String color;
+    public String[] itemsForSale, thingsToDo;
+    public String shopName, greeting, farewell, errorMessage, color;
     private Town town;
     public Bank playerAccount;
     private Scanner scan = new Scanner(System.in);
 
     /**
      * This gets all the data we will need for the merchant, including what they might sell, what they say, and what they do. 
-     * @param itemsForSale
-     * @param priceOfItem
-     * @param shopName
-     * @param thingsToDo
-     * @param greeting
-     * @param farewell
-     * @param errorMessage
-     * @param color
+     * * Basically creates the shop. 
+     * @param itemsForSale this is an array of all the items for sale
+     * @param priceOfItem this is an array which holds the pricing of all the items listed above. 
+     * @param shopName this is the name of the business
+     * @param thingsToDo this is the things u can do within a building, for example, you can buy, sell, or leave in a shop
+     * @param greeting this is what they say at the start
+     * @param farewell this is what they say as you leave
+     * @param errorMessage this is what they say to you if you say the wrong thing. 
+     * @param color this is the color of the text in the store. 
      */
     Merchant(Bank playerAccount, Town town, String[] itemsForSale, int[] priceOfItem, String shopName, String[] thingsToDo, String greeting, String farewell, String errorMessage, String color) {
+        
         this.itemsForSale = itemsForSale;
         this.priceOfItem = priceOfItem;
         this.shopName = shopName;
@@ -36,21 +33,30 @@ public class Merchant {
         this.color = color;
         this.town = town;
         this.playerAccount = playerAccount;
+
     }
 
+    /**
+     * This method runs when they walk into the store. 
+     */
     public void shop() {
+        //Greets them and asks what they want to do. 
         System.out.println("-----------------------------------------------------------");
         Printer.printColor(greeting, color);
         Printer.printColor("What would you like to do: "+ Arrays.toString(thingsToDo), color);
         String responseBack = ErrorChecker.compareArrayOfStrings(thingsToDo, errorMessage, "red").toLowerCase();
-        
+
+        //based on the possible things they can do in that store and the things listed here, we run commands. 
+        // ? better than having a million classes cus reusability?
         switch (responseBack) {
-            // * ALl things
+            // * All things
             case "leave":
                 town.characterEnteringTown();
                 break;
 
             // * SHOP THINGS
+
+            // This will list out the items we will sell and ask us to buy one or get out. 
             case "buy":
                 System.out.println("Items for sale: ");
                 for (int i = 0; i < itemsForSale.length; i++) {
@@ -58,22 +64,27 @@ public class Merchant {
                 }
                 break;
 
+            // This will allow us to sell some of the items in our inventory. 
             case "sell":
                 Printer.printColor("Here are your items and how much they are worth: ",color);
                 break;
 
+            //this will be for when we can upgrade weapons
             case "upgrade":
                 break;
 
+            //this will let us view our special talents. 
             case "view talents":
                 Printer.printColor("Here are your talents", color);
                 break;
 
+            //Will allow us to be healed. 
+            //? Maybe you can buy into health insurance to get reduced healing rates
             case "get healing":
                 Printer.printColor("Healing you: ", color);
                 break;
 
-
+            //shows bank account balance
             case "view balance":
                 Printer.printColor("Here is your current balance: " + playerAccount.getBalance(), color);
                 System.out.println("Please enter any character to continue: ");
@@ -81,6 +92,7 @@ public class Merchant {
                 shop();
                 break;
 
+            //allows you to withdraw
             case "withdraw": 
                 double amountForWithdraw = ErrorChecker.doubleWithMinAndMax(0.0,playerAccount.getBalance() + 1, "Please enter much would you like to withdraw: ", color);
                 playerAccount.withdraw(amountForWithdraw);
@@ -88,11 +100,11 @@ public class Merchant {
                 shop();
                 break;
                 
-        }
-        
-        
+        }        
     }
-
+    
+    //! MIGHT NOT BE NEEDED IF WE JUST MAKE THE SHOP NAME PUBLIC. SHOULD ASK Mr. Carby IF WE CAN JUST USE PUBLIC VARIABLES
+    //! RATHER THAN METHODS...
     public String getShopName() {
         return shopName;
     }
--- Town.java
+++ Town.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
 
 public class Town {
+    //* Variables that we will need during this code 
 
-    public static int floorLvl;
-    public static String townName;
-    // Store templates for the stores
+    //this array list will be filled with the names of all the shops in the town. 
+    ArrayList<String> namesOfThingsInTown = new ArrayList<String>();
+    Merchant bank, hospital, shop;
+    Merchant allMerchants[];
+    String townName;
+    Bank playerAccount;
+    int floorLvl;
+
+    // * INFORMATION REGARDING WHAT EACH STORE WILL CONTAIN
 
     private static String[] shopForSale = { "car", "shoe", "pizza", "macaroni" };
     private static int[] shopPrices = { 100, 10, 99, 88 };
@@ -21,7 +28,6 @@ public class Town {
     private static String hospitalGreeting = "Welcome to the hospital!";
     private static String hospitalFarewell = "Thank you for coming to the hospital I hope we enjoyed your stay. ";
     private static String hospitalErrorMessage = "Please speak clearly";
-    private static Bank playerAccount;
 
     private static String bankName = "Golden Bank Of Reign";
     static String[] thingsAtBank = { "withdraw", "view balance", "leave" };
@@ -29,28 +35,35 @@ public class Town {
     static String bankFarewell = "Thank you for coming to the bank";
     private static String bankErrorMessage = "Please speak clearly.";
 
-    Merchant bank, hospital, shop;
-    Merchant allMerchants[] = new Merchant[3];
 
-    ArrayList<String> namesOfThingsInTown = new ArrayList<String>();
-            
+
+    /**
+     * This is the creation of the town. It sets up all the merchants, links up banking, and gets all the
+     * information needed to run a town, such as its name and floor level
+     * @param townName this is the name of the town. Higher the level, the higher the prestigious town title. 
+     * @param floorLvl this is the level of the society. Higher, the more prestigious. 
+     * @param playerAccount this is the players bank account
+     */
     Town(String townName, int floorLvl, Bank playerAccount) {
+
+        //we need to pass through the players account information so that we can have a single constant account throughout 
+        //all of the classes
         this.playerAccount = playerAccount;
         this.townName = townName;
         this.floorLvl = floorLvl;
 
+        // INFORMATION ABOUT THE STORES
         bank = new Merchant(playerAccount, this, bankforsale, bankPrices, bankName, thingsAtBank, bankGreeting,
                 bankFarewell, bankErrorMessage, "red");
 
         shop = new Merchant(playerAccount, this, shopForSale, shopPrices, shopName, thingsAtShop, shopGreeting,
-        shopFarewell, shopErrorMessage, "green");
+                shopFarewell, shopErrorMessage, "green");
 
         hospital = new Merchant(playerAccount, this, hospitalForSale, hospitalPrices, hospitalName,
-        thingsAtHospital, hospitalGreeting, hospitalFarewell, hospitalErrorMessage, "purple");
+                thingsAtHospital, hospitalGreeting, hospitalFarewell, hospitalErrorMessage, "purple");
 
-        allMerchants[0] = bank;
-        allMerchants[1] = shop;
-        allMerchants[2] = hospital;
+        //initializes array of all the merchants
+        allMerchants = new Merchant[] {bank, shop, hospital};
         
         // makes a list of all the names of shops in town.
         for (int i = 0; i < allMerchants.length; i++) {
@@ -60,9 +73,11 @@ public class Town {
 
     public void characterEnteringTown() {
 
-        // welcomes the character to town and shows them what we have here
-        Printer.printColor("Welcome to " + townName + ", traveler! Here is a brochure with all you can do here! \n",
-                "blue");
+        // welcomes the character to town
+        Printer.printColor("Welcome to " + townName + ", traveler on floor " + floorLvl + "!\n"
+            + "Here is a brochure with all you can do here! \n","blue");
+
+        //prints out all the merchants in the town. 
         for (Merchant merchant : allMerchants) {
             Printer.printColor(merchant.getShopName(), "blue");
         }
@@ -71,11 +86,17 @@ public class Town {
         // gets them to enter a shop name as to where they want to go, and then runs
         // that shop.
         String whereMerchantWantsToGO = ErrorChecker.compareArrayOfStrings(
+                //makes the names of all the things in town into an actual array, then uses that to check if the user
+                //enters that value
                 namesOfThingsInTown.toArray(new String[namesOfThingsInTown.size()]),
                 "Sorry could you repeat that?", "blue");
+
+            //once we have a value, we let them shop at one of the places they asked for. 
+            //this checks which name of the shop they entered, then sends them in. 
         for (Merchant merchant : allMerchants) {
             if (whereMerchantWantsToGO == merchant.getShopName()) {
                 merchant.shop();
+                return;
             }
         }
     }
